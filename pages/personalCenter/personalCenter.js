9f6e506edd699d2c13bdfbd904e898d3c311b549
lenovo <416266475@qq.com>
2017-06-26 18:18:53 +0800

    personalcenter

---
+++ pages/personalCenter/personalCenter.js
@@ -0,0 +1,9 @@
+/**
+ * 说明：
+ */
+var personalCenterUtil = require('../../utils/personalCenterUtil.js');
+Page({
+  data : {
+    logs : []
+  },
+});
---
+++ utils/personalCenterUtil.js
@@ -0,0 +1,6 @@
+/**
+ * 说明：测试
+ */
+module.exports = {
+  personName : '王广开'
+};
